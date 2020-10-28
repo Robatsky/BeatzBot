f89d55a90bb0d7c7ca5408202d2a384d4b5e4928
Robatsky <roberthein.98.@web.de>
2020-10-28 20:01:39 +0100

    feat: added endpoint for updating prefix for guild

--- dashboard-backend/src/app.js
+++ dashboard-backend/src/app.js
@@ -31,6 +31,8 @@ app.use(cors({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.use('/api', routes);
 
--- dashboard-backend/src/routes/discord.js
+++ dashboard-backend/src/routes/discord.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const User = require('../database/schemas/User');
+const Guild = require('../database/schemas/GuildConfig');
+
 const { getBotGuilds } = require('../api/BotService');
 const { getCommonGuilds } = require('../api/GuildService');
 
@@ -12,6 +14,20 @@ router.get('/guilds', async (req, res) => {
         const commonGuilds = getCommonGuilds(userGuilds, botGuilds);
         res.send(commonGuilds);
     }
+});
+
+router.put('/guilds/:guildId/prefix', async (req, res) => {
+    const { prefix } = req.body;
+    const { guildId } = req.params;
+
+    if(!prefix) return res.status(400).send({msg: 'Prefix is required'});
+
+    const guild = await Guild.findOneAndUpdate( { guildId: guildId }, { prefix });
+
+    if(!guild) {
+        return res.status(400).send( {msg: `No guild found for the id ${guildId}`});
+    } 
+    return res.status(200).send( { msg: `Prefix has been updated successfully.` });
 });
 
 module.exports = router;
